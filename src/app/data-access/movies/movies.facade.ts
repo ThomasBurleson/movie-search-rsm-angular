b0e8ec2dadb2ef78e56caee2182009ece4251f7e
thomasburleson <thomasburleson@gmail.com>
2022-04-07 15:58:26 -0500

    lab(1): use ‘freeze()’ for immutable state

--- src/app/data-access/movies/movies.facade.ts
+++ src/app/data-access/movies/movies.facade.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { freeze } from 'immer';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { buildMatchIndicator, computeFilteredMovies } from './movies.filters';
 import { MoviesDataService } from './movies.data-service';
-import { initState, MovieItem, MovieState, MovieViewModel } from './movies.model';
+import {
+  initState,
+  MovieItem,
+  MovieState,
+  MovieViewModel,
+} from './movies.model';
 
 /**
  * Load movies and cache results for similar future calls.
@@ -20,13 +26,14 @@ export class MoviesFacade {
   public vm$: Observable<MovieViewModel>;
 
   constructor(private movieAPI: MoviesDataService) {
-    const state = initState();
+    const state = freeze(initState(), true);
     const api = {
       loadMovies: (searchBy: string) => this.loadMovies(searchBy),
       updateFilter: (filterBy: string) => this.updateFilter(filterBy),
       clearFilter: () => this.updateFilter(''),
     };
-    const addAPI = (s: MovieState): MovieViewModel => ({ ...s, ...api });
+    const addAPI = (s: MovieState): MovieViewModel =>
+      freeze({ ...s, ...api }, true);
 
     this.state = state;
     this._emitter = new BehaviorSubject<MovieState>(state);
@@ -44,10 +51,9 @@ export class MoviesFacade {
   loadMovies(searchBy: string, page = 1): Observable<MovieState> {
     this.movieAPI.searchMovies(searchBy, page).subscribe((list: unknown) => {
       const allMovies = list as MovieItem[];
+      const state = { ...this.state, allMovies, searchBy };
 
-      this.state.allMovies = allMovies;
-      this.state.searchBy = searchBy;
-
+      freeze((this.state = state), true);
       this.updateFilter(this.state.filterBy);
     });
 
@@ -58,12 +64,12 @@ export class MoviesFacade {
    * Update the filterBy value used to build the `filteredMovies` list
    */
   updateFilter(filterBy?: string) {
-    this.state.filterBy = filterBy;
-    const movies = computeFilteredMovies(this.state);
+    const state = { ...this.state, filterBy };
+    const movies = computeFilteredMovies(state);
     const matchInOverview = buildMatchIndicator(filterBy);
 
-    this.state.filteredMovies = matchInOverview(movies);
-    this._emitter.next(this.state);
+    state.filteredMovies = matchInOverview(movies);
+    this._emitter.next((this.state = freeze(state, true)));
 
     return this.vm$;
   }
