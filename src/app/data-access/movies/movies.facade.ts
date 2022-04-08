9ae85d38bd85bd1b69221a8c49f8eaa949f57412
thomasburleson <thomasburleson@gmail.com>
2022-04-08 15:09:18 -0500

    lab(3): use 'movieReducer()' and actions

--- src/app/data-access/movies/movies.facade.ts
+++ src/app/data-access/movies/movies.facade.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 
-import { produce } from 'immer';
+import { freeze, produce } from 'immer';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { buildMatchIndicator, computeFilteredMovies } from './movies.filters';
 import { MoviesDataService } from './movies.data-service';
 import {
   initState,
@@ -12,36 +11,39 @@ import {
   MovieState,
   MovieViewModel,
 } from './movies.model';
+import { movieStateReducer, actions } from './movies.reducer';
 
 /**
- * Load movies and cache results for similar future calls.
+ * Load movies and cache results for similar future calls.j
  * Architecture:
  *
  */
 
 @Injectable()
 export class MoviesFacade {
-  private state: MovieState;
+  private state: MovieState = freeze(initState(), true);
   private _emitter: BehaviorSubject<MovieState>;
   public vm$: Observable<MovieViewModel>;
 
   constructor(private movieAPI: MoviesDataService) {
-    const state = initState();
     const api = {
       loadMovies: (searchBy: string) => this.loadMovies(searchBy),
       updateFilter: (filterBy: string) => this.updateFilter(filterBy),
       clearFilter: () => this.updateFilter(''),
     };
-    const addAPI = (s: MovieState): MovieViewModel =>
-      produce<MovieState>(this.state, (s: MovieState): MovieViewModel => {
-        return { ...s, ...api };
-      }) as MovieViewModel;
+    const addAPI = (s: MovieState): MovieViewModel => {
+      return produce<MovieState>(
+        this.state,
+        (s: MovieState): MovieViewModel => {
+          return { ...s, ...api };
+        }
+      ) as MovieViewModel;
+    };
 
-    this.state = state;
-    this._emitter = new BehaviorSubject<MovieState>(state);
+    this._emitter = new BehaviorSubject<MovieState>(this.state);
     this.vm$ = this._emitter.asObservable().pipe(map(addAPI));
 
-    this.loadMovies(state.searchBy);
+    this.loadMovies(this.state.searchBy);
   }
 
   /**
@@ -51,14 +53,17 @@ export class MoviesFacade {
    * Auto-save to cache; based on specified search keys
    */
   loadMovies(searchBy: string, page = 1): Observable<MovieState> {
-    this.movieAPI.searchMovies(searchBy, page).subscribe((list: unknown) => {
-      const allMovies = list as MovieItem[];
-      this.state = produce(this.state, (draft: MovieState) => {
-        draft.allMovies = allMovies;
-        draft.searchBy = searchBy;
+    this.movieAPI
+      .searchMovies<MovieItem[]>(searchBy, page)
+      .subscribe((list: MovieItem[]) => {
+        const allMovies = list as MovieItem[];
+
+        this.state = movieStateReducer(
+          this.state,
+          actions.onSearchMovies(searchBy, allMovies)
+        );
+        this._emitter.next(this.state);
       });
-      this.updateFilter(this.state.filterBy);
-    });
 
     return this.vm$;
   }
@@ -67,15 +72,7 @@ export class MoviesFacade {
    * Update the filterBy value used to build the `filteredMovies` list
    */
   updateFilter(filterBy?: string) {
-    const state = { ...this.state, filterBy };
-    const movies = computeFilteredMovies(state);
-    const addMatchIndicators = buildMatchIndicator(filterBy);
-
-    this.state = produce(this.state, (draft: MovieState) => {
-      draft.filteredMovies = addMatchIndicators(movies);
-      draft.filterBy = filterBy || '';
-    });
-
+    this.state = movieStateReducer(this.state, actions.updateFilter(filterBy));
     this._emitter.next(this.state);
 
     return this.vm$;
--- src/app/data-access/movies/movies.reducer.ts
+++ src/app/data-access/movies/movies.reducer.ts
@@ -1,24 +1,65 @@
-import { MovieState } from './movies.model';
+import { produce } from 'immer';
 
-export interface MovieAction {
-  type: 'searchBy' | 'clearFilter';
-  payload?: string;
+import { MovieState, MovieItem } from './movies.model';
+import { buildMatchIndicator, computeFilteredMovies } from './movies.filters';
+
+interface Action {
+  type: string;
 }
 
-export function searchAction(searchBy: string): MovieAction {
-  return { type: 'searchBy', payload: searchBy };
+/**
+ * Events with `On` prefix signify activity that happens AFTER
+ * server responds [with data]. The server performs a search for a "searchMovie"
+ * action and then a "onSearchMovie" action is dispatched to update the state with the results
+ */
+interface OnSearchMovieAction extends Action {
+  type: 'onSearchMovies';
+  searchBy: string;
+  allMovies: MovieItem[];
 }
 
-export function clearFilterAction(): MovieAction {
-  return { type: 'clearFilter' };
+interface UpdateFilterAction extends Action {
+  type: 'updateFilter';
+  filterBy: string;
 }
 
-export function moviesReducer(state: MovieState, action: MovieAction) {
-  switch (action.type) {
-    case 'searchBy':
-      return state; // fixme
-    case 'clearFilter':
-      return state; // fixme
-  }
-  return state;
+// Redux/Ngrx-like ActionCreator functions
+
+export const actions = {
+  onSearchMovies(
+    searchBy: string,
+    allMovies: MovieItem[]
+  ): OnSearchMovieAction {
+    return { type: 'onSearchMovies', allMovies, searchBy };
+  },
+
+  updateFilter(filterBy: string): UpdateFilterAction {
+    return { type: 'updateFilter', filterBy };
+  },
+};
+
+// Redux/Ngrx-like Reducer functions
+
+export type MovieAction = OnSearchMovieAction | UpdateFilterAction;
+
+export function movieStateReducer(
+  state: MovieState,
+  action: MovieAction
+): MovieState {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case 'onSearchMovies':
+        const { searchBy, allMovies } = action;
+        draft.allMovies = allMovies;
+        draft.searchBy = searchBy;
+        break;
+      case 'updateFilter':
+        draft.filterBy = action.filterBy;
+        break;
+    }
+    const movies = computeFilteredMovies(draft);
+    const addMatchIndicators = buildMatchIndicator(draft.filterBy);
+
+    draft.filteredMovies = addMatchIndicators(movies);
+  });
 }
