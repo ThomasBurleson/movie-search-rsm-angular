c50daad44f86268e739d1f9792d4842bb6b9cbeb
thomasburleson <thomasburleson@gmail.com>
2022-04-08 15:26:12 -0500

    lab(4): make 'filteredMovies' a computed property

--- src/app/data-access/movies/movies.model.ts
+++ src/app/data-access/movies/movies.model.ts
@@ -6,26 +6,40 @@ export interface MovieItem {
 
 export const trackByID = (m: MovieItem) => m.poster_path;
 
+/**
+ * This state is serializable
+ */
 export interface MovieState {
   allMovies: MovieItem[];
-  filteredMovies: MovieItem[];
   searchBy: string;
   filterBy: string;
 }
 
+/**
+ * This is a simple API meant for use within the
+ * UI layer html templates
+ */
 export interface MovieAPI {
   updateFilter: (filterBy: string) => void;
   loadMovies: (searchBy: string) => void;
   clearFilter: () => void;
 }
 
+/**
+ * This is runtime 'extra' view model state
+ * that includes 'filteredMovies' since we do not
+ * want that serialized.
+ */
+export interface MovieComputedState {
+  filteredMovies: MovieItem[];
+}
+
 export function initState(): MovieState {
   return {
     allMovies: [],
-    filteredMovies: [],
     searchBy: 'dogs',
     filterBy: '',
   };
 }
 
-export type MovieViewModel = MovieState & MovieAPI;
+export type MovieViewModel = MovieState & MovieComputedState & MovieAPI;
