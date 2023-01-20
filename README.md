5c712d92eef0c174d704f0f6a3216a1886586e68
thomasburleson <thomasburleson@gmail.com>
2023-01-20 16:34:32 -0600

    chore(repo): upgrade angular

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This Angular SPA uses Tailwind, Reactive Stores, Zustand, and View Models to demonstrate the beauty and benefit of reactive stores in Angular.
 
-![](https://user-images.githubusercontent.com/210413/210111536-487fce9a-1e12-4ae1-853d-15ad50838515.png)
+[![](https://user-images.githubusercontent.com/210413/213818208-529740e4-2981-4a21-9398-7bc75e4ccf7c.png)](https://codesandbox.io/p/github/ThomasBurleson/zustand-angular-movie-search/draft/frosty-frost?file=%2Fpackage.json&selection=%5B%7B%22endColumn%22%3A47%2C%22endLineNumber%22%3A6%2C%22startColumn%22%3A47%2C%22startLineNumber%22%3A6%7D%5D)
 
 ## Background
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,51 @@
-const { createGlobPatternsForDependencies } = require("@nrwl/angular/tailwind");
-const { join } = require("path");
+const { join } = require('path');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: [
-    join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
+  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}')],
   theme: {
     extend: {
       screens: {
-        xs: "540px",
+        xs: '540px',
       },
       colors: {
-        blue: "#1fb6ff",
-        pink: "#ff49db",
-        orange: "#ff7849",
-        green: "#13ce66",
-        "gray-dark": "#273444",
-        gray: "#8492a6",
-        "gray-light": "#d3dce6",
+        blue: '#1fb6ff',
+        pink: '#ff49db',
+        orange: '#ff7849',
+        green: '#13ce66',
+        'gray-dark': '#273444',
+        gray: '#8492a6',
+        'gray-light': '#d3dce6',
       },
       fontFamily: {
-        sans: ["Graphik", "sans-serif"],
-        serif: ["Merriweather", "serif"],
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
       },
       boxShadow: {
         // Examples of custom box shadows
-        header: "inset 0px -1px 0px rgba(15, 31, 44, 0.18)",
-        footer: "inset 0px 1px 0px rgba(0, 0, 0, 0.08)",
+        header: 'inset 0px -1px 0px rgba(15, 31, 44, 0.18)',
+        footer: 'inset 0px 1px 0px rgba(0, 0, 0, 0.08)',
+      },
+      lineClamp: {
+        14: '14',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms'),
+    plugin(({ forms, addBase, addComponents, addUtilities, theme }) => {
+      // Examples of using base layer custom styles in a plugin to match designs
+      addBase({
+        h1: {
+          fontSize: '14px',
+          lineHeight: '16px',
+        },
+        p: {
+          fontSize: '10px',
+          lineHeight: '12px',
+        },
+      });
+    }),
+  ],
 };
